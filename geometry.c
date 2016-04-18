1b6cdc2f29845766b63e96961dea5d23fe66b9ec
Brennon Lee <Brennon.Lee@colorado.edu>
2016-04-18 16:55:02 -0600

    added test case for triangle area

--- geometry.c
+++ geometry.c
@@ -69,3 +69,27 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+double coord_2d_area_triangle(const coord_2d_t *a, const coord_2d_t *b, const coord_2d_t *c){
+
+ /*   if(!a){
+        DEBUG(__FILE__, __LINE__, __func__, "'a' must not be NULL");
+        return;
+    }
+    if(!b){
+        DEBUG(__FILE__, __LINE__, __func__, "'b' must not be NULL");
+        return;
+    }
+    if(!c){
+        DEBUG(__FILE__, __LINE__, __func__, "'c' must not be NULL");
+        return;
+    }
+*/
+    /*math*/
+    int by = b->y;
+    int cy = c->y;
+    int ay = a->y;
+    double area =  ((a->x) * (by-cy) + (b->x)*(cy-ay) + (c->x)*(ay-by)) / 2;
+    double Absarea = abs(area);
+    return Absarea;
+
+}
